2268252685548283b08a9763ae430935f3710c83
Anastasiia Hanuliak <anastasia.hanuliak@gmail.com>
2019-08-28 13:05:03 +0300

    add main activity logic

--- app/src/main/java/com/ahanuliak/iwatch/MainActivity.kt
+++ app/src/main/java/com/ahanuliak/iwatch/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.ahanuliak.iwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ahanuliak.iwatch.ui.MovieViewModel
+import com.ahanuliak.iwatch.ui.adapter.MovieListAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var movieViewModel: MovieViewModel
+    private lateinit var adapter: MovieListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies_to_wath)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        adapter = MovieListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        movieViewModel = ViewModelProviders.of(this).get(MovieViewModel::class.java)
+        movieViewModel.allMovies.observe(this, Observer { movies ->
+            movies?.let { adapter.setMovies(it) }
+        })
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddMovieActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        adapter.deleteChecked(movieViewModel)
+        return true
     }
 }
