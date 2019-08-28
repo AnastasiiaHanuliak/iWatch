0410940ebb197b985dd86d7b7f52818498bdd08e
Anastasiia Hanuliak <anastasia.hanuliak@gmail.com>
2019-08-28 12:13:57 +0300

    create activity classes, add to manifest

---
+++ app/src/main/java/com/ahanuliak/iwatch/AddMovieActivity.kt
@@ -0,0 +1,12 @@
+package com.ahanuliak.iwatch
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class AddMovieActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
