052a91d1a2dbcf364b456a7c3eb29edeaf2c73c0
Anastasiia Hanuliak <anastasia.hanuliak@gmail.com>
2019-08-28 13:06:40 +0300

    change add movie and search result activity

--- app/src/main/java/com/ahanuliak/iwatch/AddMovieActivity.kt
+++ app/src/main/java/com/ahanuliak/iwatch/AddMovieActivity.kt
@@ -1,12 +1,62 @@
 package com.ahanuliak.iwatch
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+import com.ahanuliak.iwatch.data.entity.Movie
+import com.ahanuliak.iwatch.ui.MovieViewModel
+import com.ahanuliak.iwatch.util.IMAGE_URL
+import com.google.android.material.textfield.TextInputEditText
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_add_movies.*
 
 class AddMovieActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var editTitleView: TextInputEditText
+    private lateinit var editReleaseYearView: TextInputEditText
+    private lateinit var imgView: ImageView
+    private lateinit var movieViewModel: MovieViewModel
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_movies)
+        editTitleView = findViewById(R.id.edit_movie_title)
+        editReleaseYearView = findViewById(R.id.edit_release_year)
+        imgView = findViewById(R.id.movie_image)
+
+        val button = findViewById<Button>(R.id.button_save)
+        movieViewModel = ViewModelProviders.of(this).get(MovieViewModel::class.java)
+
+        if ( intent.hasExtra("movie_title"))
+        {
+
+            editTitleView.setText(intent.getStringExtra("movie_title").toString())
+            editReleaseYearView.setText(intent.getStringExtra("release_year").toString())
+            Picasso.get()
+                .load(IMAGE_URL + intent.getStringExtra("poster_path").toString())
+                .into(imgView)
+        }
+
+        button.setOnClickListener {
+            val word = editTitleView.text.toString()
+            val releaseYear = editReleaseYearView.text.toString()
+            var img =""
+            if ( intent.hasExtra("poster_path")) {
+                img = intent.getStringExtra("poster_path")
+            }
+            movieViewModel.insert(Movie(word,releaseYear,img))
+            val intent = Intent(this@AddMovieActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        search_btn.setOnClickListener{
+            val intent = Intent(this@AddMovieActivity, SearchResultActivity::class.java)
+            intent.putExtra("title", editTitleView.text.toString());
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/ahanuliak/iwatch/SearchResultActivity.kt
+++ app/src/main/java/com/ahanuliak/iwatch/SearchResultActivity.kt
@@ -1,12 +1,45 @@
 package com.ahanuliak.iwatch
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ahanuliak.iwatch.ui.adapter.SearchResultAdapter
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class SearchResultActivity: AppCompatActivity(){
+    private lateinit var adapter: SearchResultAdapter
+    private var disposable: Disposable? = null
+    private val tmdbApiServe by lazy {
+        ApiServiceTMDB.create()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search_result)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.search_result_rv)
+        adapter = SearchResultAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        beginSearch(intent.getStringExtra("title"))
+    }
+
+    private fun beginSearch(searchString: String) {
+        disposable = tmdbApiServe.getMoviesByTitle(searchString)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { result -> adapter.setMovies(result.results)},
+                { error -> Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show() }
+            )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
     }
 }
